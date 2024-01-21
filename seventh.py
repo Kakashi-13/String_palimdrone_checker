5a3b33974d11b07aaf2bb544afb636fac6162f4c
Jignesh Purohit <156500596+Kakashi-13@users.noreply.github.com>
2024-01-21 14:42:04 +0530

    Add files via upload
    run seventh.py

---
+++ seventh.py
@@ -0,0 +1,12 @@
+fh = open('output7.txt','w')
+
+a = input("Enter the string : ")
+reverse = a[::-1]
+
+fh.write("The String is : " + a + '\n')
+if reverse == a:
+    fh.write("palindrone " + '\n')
+else:
+    fh.write("it's not" + '\n')
+
+fh.write("---------------------------------------------")
